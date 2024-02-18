49242c7f73547020bdc96ee86757bbfaf4414ba8
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-19 01:03:49 +0300

    add grids and colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header>
+        <div className="app-wrapper">
+            <header className="header">
+
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/2048px-ChatGPT_logo.svg.png"
                     alt="logo"/>
             </header>
-            <nav>
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>Main content</div>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
             </nav>
+            <div className="content">Main content</div>
         </div>
     );
 }
