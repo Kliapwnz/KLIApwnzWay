04929cfa689f19f439f69ba1b15ce30c6a613aab
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-19 01:05:18 +0300

    add pic to content

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,12 @@ function App() {
                 <div><a>Music</a></div>
                 <div><a>Settings</a></div>
             </nav>
-            <div className="content">Main content</div>
+            <div className="content">
+                <img
+                    src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik"
+                    alt="content"
+                />
+            </div>
         </div>
     );
 }
