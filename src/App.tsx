d9ba4f86a8c13bdde6d2f8e04ca0b839dd30d946
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-19 01:09:49 +0300

    add pic to content

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,22 @@ function App() {
                 <div><a>Settings</a></div>
             </nav>
             <div className="content">
-                <img
-                    src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik"
-                    alt="content"
-                />
+                <div>
+                    <img
+                        src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik"
+                        alt="content"
+                    />
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                    <div>Post 3</div>
+                </div>
             </div>
         </div>
     );
