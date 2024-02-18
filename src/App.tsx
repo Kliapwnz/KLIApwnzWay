5826c2a116d35445b327e8d8ea2252925e979e5a
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-19 01:20:37 +0300

    create component Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header";
-import {NavBar} from "./components/NavBar";
+import {Header} from "./components/Header/Header";
+import {NavBar} from "./components/NavBar/NavBar";
+import {Profile} from "./components/Profile/Profile";
 
 
 function App() {
@@ -9,24 +10,7 @@ function App() {
         <div className="app-wrapper">
             <Header/>
             <NavBar/>
-            <div className="content">
-                <div>
-                    <img
-                        src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik"
-                        alt="content"
-                    />
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>New post</div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                    <div>Post 3</div>
-                </div>
-            </div>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik"
+                    alt="content"
+                />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+                <div>Post 3</div>
+            </div>
+        </div>
+    );
+};
+
