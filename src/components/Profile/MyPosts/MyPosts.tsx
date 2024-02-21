2e44ce868301e24f3e40b3cded0b0af93d52a1e8
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-21 08:58:28 +0300

    Create MyPosts.tsx and MyPosts.module.css

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const MyPosts = () => {
+   return (
+      <div>
+         My posts
+         <div>New post</div>
+         <div>Post 1</div>
+         <div>Post 2</div>
+         <div>Post 3</div>
+      </div>
+   );
+};
+
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./Profile.module.css"
+import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
    return (
@@ -13,13 +14,7 @@ export const Profile = () => {
          <div>
             ava + description
          </div>
-         <div>
-            My posts
-            <div>New post</div>
-            <div>Post 1</div>
-            <div>Post 2</div>
-            <div>Post 3</div>
-         </div>
+         <MyPosts/>
       </div>
    );
 };
