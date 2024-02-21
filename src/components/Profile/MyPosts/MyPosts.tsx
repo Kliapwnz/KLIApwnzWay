4be24fd8979c0fff9f129af39567e39e3aa320d4
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-21 09:03:51 +0300

    Create Post.tsx and Post.module.css

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,10 +4,10 @@ export const MyPosts = () => {
    return (
       <div>
          My posts
-         <div>New post</div>
-         <div>Post 1</div>
-         <div>Post 2</div>
-         <div>Post 3</div>
+         <div>
+            <textarea></textarea>
+            <button>Add Post</button>
+         </div>
       </div>
    );
 };
