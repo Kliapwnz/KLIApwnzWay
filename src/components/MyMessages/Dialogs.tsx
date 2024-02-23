9ac78444cff7b752e6ef5e82d4e9592f43088bd8
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-23 10:16:21 +0300

    create dialogs

--- src/components/MyMessages/Dialogs.tsx
+++ src/components/MyMessages/Dialogs.tsx
@@ -3,8 +3,20 @@ import s from "./Dialogs.module.css"
 
 export const Dialogs = () => {
    return (
-      <div className={s.messages}>
-         messages
+      <div className={s.dialogs}>
+         <div className={s.dialogItems}>
+            <div className={s.dialog}>Sergio Aguero</div>
+            <div className={s.dialog}>Erling Haaland</div>
+            <div className={s.dialog}>David Silva</div>
+            <div className={s.dialog}>Bernardo Silva</div>
+            <div className={s.dialog}>Kyle Walker</div>
+            <div className={s.dialog}>Phil Foden</div>
+         </div>
+         <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are you?</div>
+            <div className={s.message}>Have a good day!</div>
+         </div>
       </div>
    );
 };
