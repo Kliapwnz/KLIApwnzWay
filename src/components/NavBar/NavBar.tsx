1a663f9a3fc6ea0a8a37d7fc77bb08c782dc2924
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-23 10:08:02 +0300

    looking better

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,11 +5,31 @@ import {NavLink} from "react-router-dom";
 export const NavBar = () => {
    return (
       <nav className={s.nav}>
-         <div className={s.item}><NavLink to="/profile" className={e => e.isActive ? s.active : s.item}>Profile</NavLink></div>
-         <div className={s.item}><NavLink to="/dialogs" className={e => e.isActive ? s.active : s.item}>Messages</NavLink></div>
-         <div className={s.item}><NavLink to="/news" className={e => e.isActive ? s.active : s.item}>News</NavLink></div>
-         <div className={s.item}><NavLink to="/music" className={e => e.isActive ? s.active : s.item}>Music</NavLink></div>
-         <div className={s.item}><NavLink to="/settings" className={e => e.isActive ? s.active : s.item}>Setting</NavLink></div>
+         <div className={s.item}>
+            <NavLink to="/profile"
+                     className={e => e.isActive ? s.active : s.item}>Profile
+            </NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/dialogs"
+                     className={e => e.isActive ? s.active : s.item}>Messages
+            </NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/news"
+                     className={e => e.isActive ? s.active : s.item}>News
+            </NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/music"
+                     className={e => e.isActive ? s.active : s.item}>Music
+            </NavLink>
+         </div>
+         <div className={s.item}>
+            <NavLink to="/settings"
+                     className={e => e.isActive ? s.active : s.item}>Setting
+            </NavLink>
+         </div>
       </nav>
    );
 };
