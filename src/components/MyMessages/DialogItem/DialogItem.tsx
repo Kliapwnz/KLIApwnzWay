f9cd59b21cec5e1af3e8c22699a3f5c7ba6a372b
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-24 23:59:39 +0300

    path created and used

--- src/components/MyMessages/DialogItem/DialogItem.tsx
+++ src/components/MyMessages/DialogItem/DialogItem.tsx
@@ -8,9 +8,11 @@ type DialogItemType={
 }
 
 export const DialogItem = (props:DialogItemType) => {
+   let path = "/dialogs/" + props.id
+
    return (
       <div className={s.dialog}>
-         <NavLink to={"/dialogs/" + props.id}
+         <NavLink to={path}
                   className={e => e.isActive ? s.active : s.dialog}>
             {props.name}
          </NavLink>
