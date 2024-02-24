20a30f98b91eb711523192e0d35ab024a0a65768
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-25 01:20:53 +0300

    map for posts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,16 +3,16 @@ import {Post} from "./Post/Post";
 import s from "./MyPosts.module.css"
 
 let postsData = [
-   {id:1, text:"How are you, guys?", likesCount:81},
-   {id:2, text:"My second post", likesCount:12},
-   {id:3, text:"Hello all", likesCount:4},
+   {id: 1, text: "How are you, guys?", likesCount: 81},
+   {id: 2, text: "My second post", likesCount: 12},
+   {id: 3, text: "Hello all", likesCount: 4},
 ]
 
 
 export const MyPosts = () => {
    return (
       <div className={s.postsBlock}>
-        <h3>My posts</h3>
+         <h3>My posts</h3>
          <div>
             <div>
                <textarea></textarea>
@@ -22,7 +22,7 @@ export const MyPosts = () => {
             </div>
          </div>
          <div className={s.posts}>
-            {postsData.map(el=> <Post text={el.text} likesCount={el.likesCount}/>)}
+            {postsData.map(el => <Post text={el.text} likesCount={el.likesCount}/>)}
          </div>
       </div>
    );
